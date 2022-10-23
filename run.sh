d57f7c0126bc150b8520c0d0821f5ec8bd85dd93
bodybreaker <bodybreaker@naver.com>
2022-10-23 19:22:40 +0900

    'test'

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ conda activate aicop
 
 cat /dev/null > nohup.out
 
-kill -9 $(ps aux |awk '/main.py/ {print $2}')
+kill -9 $(ps -ef | grep main.py | head -1 | awk '{print $2}')
 
 nohup python main.py &
 tail -f nohup.out

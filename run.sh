c22103f85ff7ec280e881a0f525eb6bec3f4805b
bodybreaker <bodybreaker@naver.com>
2022-10-23 18:10:34 +0900

    add shell

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 source /home/itrnd/anaconda3/etc/profile.d/conda.sh
 conda activate aicop
+
+$kill $(ps aux |awk '/main.py/ {print $2}')
+
 cat /dev/null > nohup.out
 nohup python main.py &

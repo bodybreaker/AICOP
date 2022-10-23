d878924bc9d02d7be45c1708512fc4a61516c32c
bodybreaker <bodybreaker@naver.com>
2022-10-23 18:11:04 +0900

    test

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 source /home/itrnd/anaconda3/etc/profile.d/conda.sh
 conda activate aicop
 
-$kill $(ps aux |awk '/main.py/ {print $2}')
+kill $(ps aux |awk '/main.py/ {print $2}')
 
 cat /dev/null > nohup.out
 nohup python main.py &

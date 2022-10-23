999f1fc88674fdc3cbf173d4f8a4e28d3af29d4c
bodybreaker <bodybreaker@naver.com>
2022-10-23 19:04:23 +0900

    edit shell

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 source /home/itrnd/anaconda3/etc/profile.d/conda.sh
 conda activate aicop
 
-kill $(pgrep -f 'python test.py')
+kill $(pgrep -f 'python main.py')
 
 cat /dev/null > nohup.out
 nohup python main.py &
